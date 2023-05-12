a820ad30be7c9a06ae70bb5bac5d0d20776ed186
Nascimento033 <133441015+Nascimento033@users.noreply.github.com>
2023-05-12 20:43:53 -0300

    Código conta

---
+++ CONTA.java
@@ -0,0 +1,21 @@
+public class CONTA {
+    public int numero;
+    public String nomeagencia;
+    public int numeroagencia;
+    public String nomebanco;
+    public int numerobanco;
+    public Strig tipodeconta;
+    public float saldo;
+    public void abrir() {
+    }
+    public void encerrar() {
+    }
+    public void consultar() {
+    }
+    public void saldo() {
+    }
+    public void creditar() {
+    }
+    public void debitar() {
+    }
+}
